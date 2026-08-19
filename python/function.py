72f4f0288a5464d95c6ea967ffc49569b55f3633
T124 <T124@CIPL-ATC00277.colanonline.local>
2026-08-19 19:19:35 +0530

    python function

---
+++ python/function.py
@@ -0,0 +1,26 @@
+# def add(a,b):
+#     return a * b
+# result = add(10, 5)
+# print(result)    
+
+# def introduce(name, age, city):
+#     return ("Name:", name, "Age:", age, "City:", city)
+# result = introduce("Saad", 22, "Chennai")
+# print(result)    
+
+
+# def student(name, age):
+#     print(name, age)
+# student("Saad", 22) 
+
+
+# def calculate(a, b):
+#     add = a + b
+#     sub = a - b
+#     multi = a * b
+#     div = a / b
+#     return add,sub,multi,div
+# result = calculate(10,5)
+# print(result)    
+
+
